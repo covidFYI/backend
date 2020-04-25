9db1f8f69e437e3338b3c472fe83f72a5d169722
Manan <gouhari21@hotmail.com>
2020-04-25 18:07:48 +0530

    Added news scraping and api. Also stats api.

--- app/data/controller.py
+++ app/data/controller.py
@@ -4,10 +4,11 @@ from app.extensions import cache, mongo
 from flask import jsonify, request, make_response, abort, Response
 from flask_caching import Cache
 import json
-
-CACHE_TIMEOUT = 600*1 # 10 min
+from random import shuffle
+CACHE_TIMEOUT = 1*1 # 10 min
 
 entries_collection = mongo.db.entries
+news_collection = mongo.db.news
     
 @data_bp.route('/api/v1/states')
 @cache.cached(timeout=CACHE_TIMEOUT, key_prefix='states')
@@ -20,6 +21,15 @@ def states():
     })
 
 
+@data_bp.route('/api/v1/news')
+@cache.cached(timeout=CACHE_TIMEOUT, key_prefix='news')
+def news():
+    news = news_collection.find({}, {'_id': 0})
+    news = tuple(news)
+    return jsonify({
+        'result': news 
+    })
+
 @data_bp.route('/api/v1/categories')
 @cache.cached(timeout=CACHE_TIMEOUT, key_prefix='categories')
 def infotypes():
@@ -29,12 +39,36 @@ def infotypes():
         'result': entries
     })
 
+@data_bp.route('/api/v1/categories/total')
+@cache.cached(timeout=CACHE_TIMEOUT, key_prefix='total_categories')
+def infotypes_total():
+    pipeline = [{"$group": {"_id": "$category", "total": {"$sum": 1}}}]
+    total = entries_collection.aggregate(pipeline) 
+
+    res = tuple({'category':x['_id'], 'total': x['total']} for x in total)
+    return jsonify({
+        'result': res
+    })
+
+
+@data_bp.route('/api/v1/categories/<category>/total')
+@cache.cached(timeout=CACHE_TIMEOUT, key_prefix='total_categories')
+def infotypes_total_state(category):
+    """ pipeline = [{"$match" : { "$category" : category} },
+                {"$group": {"_id": "$category", "total": {"$sum": 1}}}]
+    total = entries_collection.aggregate(pipeline)
+    print(category)
+    res = tuple({'category': x['_id'], 'total': x['total']} for x in total)
+    return jsonify({
+        'result': res
+    }) """
+    return 'implement this.'
+
 @data_bp.route('/api/v1/state/<string:state>')
 @cache.memoize(timeout=CACHE_TIMEOUT)
 def state_view(state):
 
     entries = entries_collection.find({'state': state}, {'_id':0})
-    entries = tuple(entries)
     return jsonify({
         'result': entries
     })
--- app/data/utils.py
+++ app/data/utils.py
@@ -3,7 +3,8 @@ from app.extensions import mongo
 from dotenv import load_dotenv
 from requests import get
 from json import loads
-from json.decoder import JSONDecodeError                 
+from json.decoder import JSONDecodeError  
+from bs4 import BeautifulSoup               
 import os
 import time #TODO: remove in prod
 import math #TODO: remove in prod
@@ -66,6 +67,110 @@ def extract_data():
 
     return all_entries
 
+def scrape_indiatoday():
+    res = []
+    l = "https://www.indiatoday.in/coronavirus-covid-19-outbreak"
+    data = get(l)
+    soup = BeautifulSoup(data.content, "lxml")
+    news_divs = soup.find_all("div", {"class": "catagory-listing"})
+    for news in news_divs:
+        title = news.h2.text.strip()
+        alink = news.a
+        if news.img:
+            img = news.img
+            img = img.get('src')
+            img = img.replace('170x96', '647x363')
+        else:
+            img = ''
+        try:
+            res.append({'title': title, 'link': 'https://www.indiatoday.in'+alink.get('href'),'img': img})
+        except:
+            continue
+    return res
+
+def scrape_ABP():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+    }
+    resp = get('https://news.abplive.com/search?s=coronavirus', headers=headers)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    news = soup.findAll('a', {'class': 'news_featured'})
+
+    res = []
+    for article in news:
+        img_src = article.find('img')['src']
+        title = article.find(
+            'div', {'class': 'news_content'}).findChild('p').text.strip()
+        link = article['href']
+        if 'Deaf And Abled People' not in title:
+            res.append({
+                'title': title,
+                'img': img_src,
+                'link': link
+            })
+    return res
+
+def scrape_NN():
+    res = []
+    headers = headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+            }
+    resp = get('https://www.newsnation.in/topic/coronavirus-news', headers=headers)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    news = soup.findAll('div', {'class': 'col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 mt-2'})
+    # col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 mt-2
+    for article in news:
+        article_container = article.findChild('a')
+        relative_link = article_container['href']
+        link = f"https://www.newsnation.in{ relative_link }"
+        img_src = article_container.findChild('div', {'class': 'position-relative'}).findChild('img')['src']
+        title = article_container.findChild('h3').text.strip()
+        res.append({
+            'title': title,
+            'link' : link,
+            'img'  : img_src
+        })
+
+    return res
+
+def scrape_ani():
+    res = []
+    anilink = 'https://www.aninews.in/topic/coronavirus/'
+    ani = get(anilink)
+    soup = BeautifulSoup(ani.content, 'html.parser')
+    anidiv = soup.find_all('div', class_='card')
+    for b in anidiv:
+        if(b.img):
+            aniimg = b.img
+            aniimg = aniimg.get('data-src')
+            aniimg = aniimg.replace('/__sized__/', '/')
+            aniimg = aniimg.replace('-thumbnail-320x180-70.', '.')
+        else:
+            aniimg = ''
+        title = b.h6
+        try:
+            title = title.text
+        except:
+            continue
+        aani = b.a
+        try:
+            aani = aani.get('href')
+            aani = 'https://www.aninews.in'+aani
+        except:
+            continue
+        res.append({'title': title, 'link': aani, 'img': aniimg})
+    return res
+
+
+def scrape_news():
+    
+    res = scrape_indiatoday()
+    res.extend(scrape_ani())
+    res.extend(scrape_ABP())
+    res.extend(scrape_NN())
+
+    return res
+
 def extract_and_import_db():
     s = time.time() # to measure how long this takes.
     entries = extract_data() # gets all the entries from Google Sheet
@@ -73,12 +178,21 @@ def extract_and_import_db():
     delete_entries_db() # delete existing entries
     entries_collection = mongo.db.entries
     entries_collection.insert_many(entries)
+    news = scrape_news()
+    print('news scraped')
+    news_collection = mongo.db.news
+    print('collection found')
+    
+    news_collection.insert_many(news)
+    print('news added.')
     end = time.time()
     print("Entries added!")
     print(f'Time taken {math.floor((end-s))}secs') # remove this in prod.
 
 def delete_entries_db():
     entries = mongo.db.entries
+    news = mongo.db.news
+    news.delete_many({})
     entries.delete_many({})
 
 if __name__ == '__main__':
